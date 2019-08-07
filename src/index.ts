149b4e6c38632c7b014b4cd7f48b120c2e9c04d0
Nick Fujita <nickfujita@gmail.com>
2019-08-07 15:22:05 +0900

    update dependencies, only include core and neo, remove client package, expose core methods

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
 import o3dapi from 'o3-dapi-core';
 import o3dapiNeo from 'o3-dapi-neo';
-import o3dapiNeoClient from 'o3-dapi-neoclient';
 
 o3dapi.initPlugins([o3dapiNeo]);
 
-o3dapi.NEO.setClientPlugin(o3dapiNeoClient);
+const {
+  utils,
+  openO3,
+  onReady,
+  setReactNativeOverrides,
+  setNodeJSOverrides,
+} = o3dapi;
 
 export default {
   ...o3dapi.NEO,
-  utils: o3dapi.utils,
+  utils,
+  openO3,
+  onReady,
+  setReactNativeOverrides,
+  setNodeJSOverrides,
 };
